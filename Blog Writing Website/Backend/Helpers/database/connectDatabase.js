acafed47171f26803c0e229f206fd837f5b71627
Manish-77 <manishathlete56@gmail.com>
2024-05-23 03:56:26 +0530

    Blog Writing

---
+++ Blog Writing Website/Backend/Helpers/database/connectDatabase.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const connectDatabase =async  () => {
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/kaka' ,{useNewUrlParser : true})
+    console.log("MongoDB Connection Successfully")
+
+} 
+
+module.exports = connectDatabase
